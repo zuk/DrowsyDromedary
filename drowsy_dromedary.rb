221e99feff1189f7cc115aab21afc381b45e9be6
Matt Zukowski <matt@zukowski.ca>
2013-04-05 19:04:21 -0400

    don't try to override 'id' attribute during save

--- drowsy_dromedary.rb
+++ drowsy_dromedary.rb
@@ -241,6 +241,9 @@ class DrowsyDromedary < Grape::API
           error!("Item #{params[:id].inspect} doesn't exist in #{params[:collection].inspect}!", 404)
         end
 
+        data['_id'] = id
+        data.delete 'id'
+
         item.merge!(data)
 
         @db.collection(params[:collection]).save(item)
